9fbd1529c4fc7ee47986f8d40042f7a25dcf7e32
ShreemBrzee06 <makwanapunammk@gmail.com>
2023-05-02 21:58:02 +0100

    done

---
+++ Test5/src/example5/Q1.java
@@ -0,0 +1,14 @@
+package example5;
+/*Length and breadth of a rectangle are 5 and 7 respectively. Write a program to calculate the area and perimeter of the rectangle.
+ */
+public class Q1
+{
+    public static void main(String[] args)
+    {int a = 5; int b= 6;
+
+        System.out.println("Area of an Rectangle: " + (a*b)+"\nPerimeter of an Rectange: " + (2*(a+b)));
+
+    }
+
+
+}
